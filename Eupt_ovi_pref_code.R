ab3d37ae0b861eb294b153c208e5a4f8dc422028
Chris Hamm <chamm@ku.edu>
2015-05-12 17:05:20 -0600

    Updated code to draw from indep mcmc runs

--- Eupt_ovi_pref_code.R
+++ Eupt_ovi_pref_code.R
@@ -5,7 +5,7 @@ set.seed(1234)
 library("bayespref")
 library("coda")
 
-# load("Eupt_ovi_pref.RData")
+# load("Data/Eupt_ovi_pref.RData")
 
 setwd("~/Desktop/Projects/Euptychia_ovi_pref")
 
@@ -17,7 +17,7 @@ head(Eupt_Sel)
 Eupt_Sel <- Eupt_Sel[which(rowSums(Eupt_Sel) > 0), ]
 Eupt_Sel
 
-Eupt_run1 <- bayesPref(pData = Eupt_Sel, mcmcL = 1e4, pops = FALSE)
+Eupt_run1 <- bayesPref(pData = Eupt_Sel, mcmcL = 1e3, pops = FALSE)
 str(Eupt_run1)
 names(Eupt_run1[[1]])
 
@@ -35,4 +35,23 @@ plot(postburn, pch = 19, las = 1)
 effectiveSize(postburn)
 autocorr.plot(postburn, lag.max = 50)
 
-# save.image("Eupt_ovi_pref.RData")
+# Running so the last state of a 1000 step run is saved for each of X runs
+SpiderMonkey <- function(N){
+	Name <- numeric(length = N)
+		for(i in 1:N){
+	temp <- bayesPref(pData = Eupt_Sel, mcmcL = 1e3, pops = FALSE)
+	Name[i] <- temp[[1]]$PopPref[2, 1000]
+	cat("\n", i, "of", N, "\n")}
+	return(Name)
+	}
+
+Runt <- SpiderMonkey(N = 1e4)
+
+
+
+
+
+
+
+
+# save.image("Data/Eupt_ovi_pref.RData")
---
+++ README.md
@@ -0,0 +1,4 @@
+Code for Hamm and Fordyce analysis of Euptychia westwoodi oviposition preference at Tirimbina, Costa Rica
+
+12 May 2015
+Initial commit
